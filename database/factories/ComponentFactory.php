5b7a7afdc3403e53b144bcb1958c2da6dd2d1d3c
Adedayo Adedapo <adedayo.adedapo@airangel.com>
2023-08-09 10:36:48 +0100

    Add new files

---
+++ database/factories/ComponentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Component>
+ */
+class ComponentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+            'name' => $this->faker->unique()->word,
+            'description' => $this->faker->sentence,
+            'quantity' => $this->faker->numerify
+        ];
+    }
+}
