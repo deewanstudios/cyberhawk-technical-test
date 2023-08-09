3e485785fbe88c09feee43f8f6c33cd01e09897d
Adedayo Adedapo <adedayo.adedapo@airangel.com>
2023-08-09 10:35:58 +0100

    Modified file names, populated factory and migration file with column names and expected types

--- database/migrations/2023_08_08_181514_create_components_table.php
+++ database/migrations/2023_08_08_181514_create_components_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,9 @@ return new class extends Migration
     {
         Schema::create('components', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('description', 255);
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
