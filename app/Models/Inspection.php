92440547fb5651f1999cb971a87a4974ddd6c700
Adedayo Adedapo <adedayo.adedapo@airangel.com>
2023-08-12 15:15:42 +0100

    Updates
    	--	Defined migarations columns
    	--	Add fillable properties to mode
    	--	Defined factory object properties

---
+++ app/Models/Inspection.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Inspection extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'turbine_id',
+        'ispected_date',
+        'grade'
+    ];
+}
