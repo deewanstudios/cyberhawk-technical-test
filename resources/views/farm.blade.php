74f17a03286d3da5536cb0c5bf50c5842bb17e37
Adedayo Adedapo <ade.adedapo9@gmail.com>
2023-08-21 09:43:47 +0100

    Removed dump statement

--- resources/views/farm.blade.php
+++ resources/views/farm.blade.php
@@ -434,7 +434,6 @@
             @php
             $link =Request::path();
             @endphp
-            @dump($link)
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
