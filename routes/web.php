d2d35c020a0c0a5bddc59320441ee8cfe84c9e38
Adedayo Adedapo <ade.adedapo9@gmail.com>
2023-08-21 15:13:58 +0100

    Hot-fix: Add redirect to farms url, when the site loads up on the front end

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect('/farms');
+});
+
 Route::prefix('farms')->group(function () {
 
     Route::get('/', [FarmController::class, 'index']);
