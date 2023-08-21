2a26698053f33bef1504dfbbb07c0cd5d03b64c7
Adedayo Adedapo <ade.adedapo9@gmail.com>
2023-08-21 09:44:42 +0100

    Removed dunp statement

--- resources/views/farm-turbines.blade.php
+++ resources/views/farm-turbines.blade.php
@@ -431,7 +431,6 @@
                 @endauth
             </div>
             @endif
-            @dump(Request::path())
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
